d771b7cb3f4384f5f303c3c01d9b60d4e589bf7f
Botato <51982229+Botato300@users.noreply.github.com>
2022-12-27 16:15:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,24 @@
 
 int main()
 {
-	ITool* p = new Pen;
-	p->acquire();
-	p->write();
-	p->release();
+	ITool* pen = createPen(10, 15);
+	pen->write();
+
+	ITool* &pen_ref = pen;
+	pen_ref->acquire();
+	pen_ref->write();
+
+	ITool* &pen_ref2 = pen;
+	pen_ref2->acquire();
+	pen_ref2->write();
+
+	ITool* &pen_ref3 = pen;
+	pen_ref3->acquire();
+	pen_ref3->write();
+
+	pen_ref->release();
+	pen_ref2->release();
+	pen_ref3->release();
 
 	return 0;
 }
