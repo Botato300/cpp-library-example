ed8af254d4f5da487e9a278ac4ba3a18f50c2339
Botato <51982229+Botato300@users.noreply.github.com>
2022-12-27 16:16:05 -0300

    Update tool.cpp

--- tool.cpp
+++ tool.cpp
@@ -1,77 +1,52 @@
 #include "tool.h"
 
-/* Definiciones de Pen */
 Pen::Pen(void)
 {
-	;
+	std::cout<<"Pen created."<<std::endl;
+	posX = 0;
+	posY = 0;
+	ref_count = 0;
+}
+
+Pen::Pen(int posX, int posY)
+{
+	std::cout<<"Pen created."<<std::endl;
+	this->posX = posX;
+	this->posY = posY;
+	ref_count = 0;
 }
 
 Pen::Pen(const Pen &p)
 {
-	;
+	posX = p.posX;
+	posY = p.posY;
+	ref_count = 0;
 }
+
+Pen& Pen::operator=(const Pen&) = default;
 
 Pen::~Pen(void)
 {
-	std::cout<<"Destroying Pen..."<<std::endl;
-}
-
-Pen& Pen::operator=(const Pen &op)
-{
-	return *this;
+	std::cout<<"Pen destroyed."<<std::endl;
 }
 
 void Pen::write(void)
 {
-	std::cout<<"Writing with Pen..."<<std::endl;
+	std::cout<<"Writing with Pen... (at coordinate "<<posX<<"x,"<<" "<<posY<<"y)\n"<<std::endl;
 }
-
 void Pen::acquire(void)
 {
-	++count;
+	++ref_count;
+	std::cout<<"A reference to Pen has been created. Current references: "<<ref_count<<std::endl;
 }
 
 void Pen::release(void)
 {
-	--count;
-	if(count == 0) delete this;
+	--ref_count;
+	std::cout<<"A reference to Pen has been deleted. Current references: "<<ref_count<<std::endl;
+	if(ref_count == 0) delete this;
 }
 
-//---------------------------------------------------------
-
-/* Definiciones de Pencil */
-Pencil::Pencil(void)
-{
-	;
-}
-
-Pencil::Pencil(const Pencil &p)
-{
-	;
-}
-
-Pencil::~Pencil(void)
-{
-	std::cout<<"Destroying Pencil..."<<std::endl;
-}
-
-Pencil& Pencil::operator=(const Pencil &op)
-{
-	return *this;
-}
-
-void Pencil::write(void)
-{
-	std::cout<<"Writing with Pencil..."<<std::endl;
-}
-
-void Pencil::acquire(void)
-{
-	++count;
-}
-
-void Pencil::release(void)
-{
-	--count;
-	if(count == 0) delete this;
+ITool* createPen(int posX, int posY){
+	return (ITool*) new Pen(posX, posY);
 }
